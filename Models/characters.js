0aa01e6b61fc3f6b85b935bb461bc48ea83eb5ae
Kayce Hubbard <ghuleh@Nameless-Ghuleh.local>
2023-01-27 17:36:13 -0600

    models

--- Models/characters.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const characterSchema = new Schema ({
-    name: {type: String, required: true},
-})
-
-const Character = mongoose.model('Character', characterSchema);
-module.exports = Character;
