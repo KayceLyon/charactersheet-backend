fcbbe7a16dc177fadda7d2697a6728a6e6445694
Kayce Hubbard <ghuleh@Nameless-Ghuleh.local>
2023-01-27 15:58:03 -0600

    Server added

---
+++ server.js
@@ -0,0 +1,22 @@
+//  DEPENDENCIES
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express()
+const session = require('express-session');
+
+app.use(express.json());
+app.use(cors());
+
+let PORT = 3000;
+if(process.env.PORT){
+	PORT = process.env.PORT
+}
+
+app.listen(PORT, ()=>{
+    console.log('Listening')
+})
+
+mongoose.connect('mongodb+srv://klyon:rEX42p0J9UVnLCEA@sheet.wxrwmjd.mongodb.net/?retryWrites=true&w=majority', () => {
+    console.log('Linked')
+})
