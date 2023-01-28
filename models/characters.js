40bb8aaa7a830e1119c7ff3188b7a84660312bf9
Kayce Hubbard <ghuleh@Nameless-Ghuleh.local>
2023-01-28 14:33:27 -0600

    Added to schema

--- models/characters.js
+++ models/characters.js
@@ -3,7 +3,12 @@ const Schema = mongoose.Schema;
 
 const characterSchema = new Schema ({
     name: {type: String, required: true},
-    class: {type: String, required: true}
+    class: {type: String, required: true},
+    level: {type: Number, required: true},
+    background: {type: String, required: true},
+    race: {type: String, required: true},
+    alignment: {type: String},
+    
 })
 
 const Character = mongoose.model('Character', characterSchema);
