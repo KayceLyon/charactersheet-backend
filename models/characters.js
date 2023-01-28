81c84266f0e839be87b6a0848ae5e4c289cd9fd2
Kayce Hubbard <ghuleh@Nameless-Ghuleh.local>
2023-01-28 14:10:00 -0600

    Update schema

--- models/characters.js
+++ models/characters.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const characterSchema = new Schema ({
     name: {type: String, required: true},
+    class: {type: String, required: true}
 })
 
 const Character = mongoose.model('Character', characterSchema);
